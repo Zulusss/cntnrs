572730bfe7db3b1c3918584c2b463ff82925fda0
Denis <grivazol@gmail.com>
2025-02-23 12:42:25 +0700

    deleted some unusable commented lines

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -39,43 +39,18 @@ public:
 
         Node(T value) : data(value), left(nullptr), right(nullptr), prev(nullptr), next(nullptr), height(1) {}
     };
-    // int num;
-    
+       
     class My_Iterator {
         public:
         
         My_Iterator() : currentNode(nullptr) {}
 
         My_Iterator(Node* startNode) : currentNode(startNode) {
-            // Node* My_Node = startNode;
-            // linkNodes_backward(My_Node);
-        }
 
-        My_Iterator(AVLTree<T>& tree, size_t position) {
-            // Используем метод дерева для инициализации currentNode
-            currentNode = tree.findKthNode(tree.root, position);
         }
 
         My_Iterator(int number) : My_Iterator{currentNode} {
-            // linkNodes(currentNode);
-            // currentNode->height = num;
-            // num = number;
-            // linkNodes(currentNode);
-            // linkin_park(currentNode);
-            // if(currentNode->prev)   currentNode   =  currentNode->prev;
-            // this = *root;
-            // if(!currentNode) std::cout << "no" << std::endl;
-            // else
-            // while(currentNode->prev) currentNode = currentNode->prev;
-            // currentNode = findKthNode();
-            // root.finder(num);
-            // My_Iterator it = begin();
-            // currentNode->
-            // currentNode = root;
-            // position = num;
-            // finder(num, currentNode);
-            // currentNode = findKthNode(root, num);
-            // currentNode = res; 
+            //for erase
         }
         
         bool hasNext() {
@@ -83,10 +58,7 @@ public:
             return !parentStack.empty();
         }
 
-        // void erase(){
-
-        // }
-
+ 
         T& operator*() const {
             return currentNode->data;
         }
@@ -96,8 +68,6 @@ public:
           }
 
           My_Iterator& operator--() {
-            // Node* ret = currentNode->prev ? currentNode->prev : currentNode;
-            // Node* ret = nullptr;
             if(currentNode->prev)
             return *this = currentNode->prev;
             else return *this;
@@ -135,10 +105,6 @@ public:
                         // if(!is_right)                    
                         ret = currentNode;
                         std::cout << ret->data << " from first time ++ " << std::endl;
-                        // if(ret. )
-                        // ret = nodeStack.top();
-                        // else std::cout << "empty stack" << std::endl;
-                        // std::cout << ret->data << " from stack ++ " << std::endl;
                     }
                     else {
                         std::cout << currentNode->data << " ret" << std::endl;
@@ -154,12 +120,6 @@ public:
             
             // return ret;
             return *this;
-
-            // if (!currentNode) {
-            //     throw std::out_of_range("Iterator out of range");
-            // }
-            // currentNode = currentNode->next;
-            // return *this;
         }
 
         bool operator!=(const My_Iterator& other) const {
@@ -178,22 +138,6 @@ public:
     class HateCrewIterator{
 
     };
-    // Iterator Begin() {
-
-    //     // if (!root) return Iterator(nullptr);
-    //     // Node* start = root;
-    //     // while (start->left) {
-    //     //     start = start->left;
-    //     // }
-    //     // linkNodes(root);
-    //     Iterator ret = Iterator(root);
-    //     ++ret;
-    //     return ret;
-    // }
-
-    // Iterator End() {
-    //     return Iterator(nullptr);
-    // }
 
     void linkNodes(Node* node) {
         static Node* lastVisited = nullptr;
@@ -692,14 +636,6 @@ void finder(size_t num){
     My_Iterator ret{res};
 }
 
-// void erase(My_Iterator pos){
-//     // pos = My_Iterator::position;
-//     if(pos < 1 || pos > how_much) std::cout << " Out of index " << std::endl;
-//     else{
-//         Node* victim = findKthNode(root, pos);
-//         root = remove(root, victim->data); 
-//     }
-// }
 void erase(My_Iterator pos) {
     // if(number) std::cout << " This is it " << std::endl;
     if (pos.currentNode == nullptr) {
@@ -714,23 +650,6 @@ void erase(My_Iterator pos) {
     root = remove(root, valueToRemove);
 }
 
-// void erase(My_Iterator pos) {
-//     My_Iterator tmp{root};
-//     if(tmp!=pos){ 
-//     tmp++;} std::cout << "*tmp   " << *tmp << std::endl;
-    
-//     // remove(pos.currentNode, pos.currentNode->data);
-//     // Получаем позицию из итератора
-//     // My_Iterator   pos = nullptr;
-//     // size_t index = findIndex(pos);
-    
-//     // if (index < 1 || index > how_much) {
-//     //     std::cout << " Out of index " << std::endl;
-//     // } else {
-//     //     Node* victim = findKthNode(root, index);
-//     //     root = remove(root, victim->data); 
-//     // }
-// }
 
 // Метод для нахождения индекса из итератора
 size_t findIndex(My_Iterator pos) {
@@ -800,15 +719,8 @@ int main() {
         // origin.insert(i);
         // origin.insert(rand() % 1000);
     }
-    // tree.erase(3)
-    // IntPrintVisitor guest;
-    // AVLTree<int>::Iterator iter; 
-    // AVLTree<int>::Iterator iter2(oak.root);
-    //AVLTree<int>::iterator Roman;
+
     auto Roman = oak.begin();
-    // for(auto i: oak)
-    // std::cout << i << std::endl;
-    // ++Roman;
     
     std::cout << *Roman << "   Roman   " <<std::endl;
     ++Roman;
@@ -832,83 +744,18 @@ int main() {
     for(auto narcoMan = pinus.begin(); narcoMan != pinus.end(); narcoMan++)
         {std::cout << *narcoMan << std::endl;}
     
-        for(auto narcoMan2 = pinus.end(); narcoMan2 != pinus.begin(); narcoMan2--)
+    for(auto narcoMan2 = pinus.end(); narcoMan2 != pinus.begin(); narcoMan2--)
         {std::cout << *narcoMan2 << std::endl;}
     
-      
-    // PrintVisitor<int> guest;
- 
-    // //std::string end_of_binarytree_strings = tree.maxdata();
-    // //std::cout << "  end of strings   " << end_of_binarytree_strings << std::endl;
     std::cout<< std::endl;
-    
-
-    // for(size_t i = 1; i <= oak.size(); i++){
-    //     oak.accept(guest, i);
-    // }
-
-    // // tree.accept(printVisitor); // Используем посетителя для обхода дерева
-    // std::cout<< std::endl;
-    // int finish = oak.maxdata();
-
-
-    // std::cout<< finish << "   last " << std::endl;
-
-    // oak.dump4(oak.root);
-    //tree.dump4(tree.root);
-    //tree.print();
-    // std::cout << std::endl;
-
-    // AVLTree<int> tree2;
-    // AVLTree<int> pinus = {3, 4, 5, 3, -999, 111};
-    // // std::set<std::string> origin;
-    // std::set<int> origin;
-    // for (size_t i = 1; i < 49; i++)
-    // {
-    //     // /* code */tree2.insert(rand() % 1000);
-    //     tree2.insert(i);
-    //     origin.insert(i);
-    //     // origin.insert(rand() % 1000);
-    // }
-    // std::cout << origin.size() << "   original size    "  << std::endl;
-    // std::cout << tree2.size() << "    size    "  << std::endl;
-    // // tree2.dump4(tree2.root);
-    // std::cout << " min " << tree2.mindata() <<  std::endl; 
-    // std::cout << " empty? " << tree2.empty() <<  std::endl; 
-    // AVLTree<int>::SetIterator it(tree2.root);
-    // while (it.hasNext()) {
-    //     std::cout << it.next() << " ";
-    // }
-    // std::cout << endl;
-    // AVLTree<int>::NewIdeaIterator fck(tree2.root); // Correct initialization
-
-
-    // std::set<int>::iterator original;
     
     AVLTree<int>::My_Iterator fck;
     fck = pinus.findmin(pinus.root);
     std::cout << *fck << "  min  " << std::endl;
     fck = pinus.findmax(pinus.root);
     std::cout << *fck << "  max  " << std::endl;
-    // AVLTree<int>::NewIdeaIterator tst;
-    // tst = tree2.begin();
-    // std::cout << *tst << "  begin ura!!! it works!!! " << std::endl;
-    // tst = tree2.end();
-    // std::cout << *tst << "  end it works!!! " << std::endl;
-    // // size_type r;
-    // // std::cout << fck
-    // std::cout << *original << "  begin  " << std::endl;
-    // original = origin.end();
-    // std::cout << *original << "  end  " << std::endl;
 
     std::cout << pinus.size() << "  size    " << std::endl;
-    // std::cout << origin.size() << "   original size    " << std::endl;
-    // // AVLTree<int> copy_tree_init  = {3,4,5};
-    // // copy_tree_init.dump4(copy_tree_init.root);
-    // // copy_tree = tree2;
-    // // std::cout << copy_tree.size() << " copy_size    " << std::endl;
-
-    // // tree2.dump4(tree2.root);
 
     fck = pinus.find(0);
     std::cout << *fck << "  find 0  " << std::endl;
@@ -921,63 +768,10 @@ int main() {
     res = pinus.contains(5);
     std::cout  << res << "     contains  5" << std::endl;
     
-    // pinus.erase(pinus.begin());
     pinus.erase(5);
     pinus.print();
     pinus.erase(5);
     pinus.dump4(pinus.root);
-    // oak.dump4(oak.root);
-
-    // // tree2.dump4(tree2.root);
-    // auto resa = origin.insert(3);
-    // std::cout << "Origin: " << *resa.first << ", True: " << resa.second << std::endl;
-    // auto resa2 = tree2.insert(3);
-    // std::cout << "My: " << *resa2.first << ", True: " << resa2.second << std::endl;
-    // std::cout << tree2.size() << "  size    " << std::endl;
-    // std::cout << origin.size() << "   original size    " << std::endl;
-
-    // resa = origin.insert(333);
-    // std::cout << "Origin2: " << *resa.first << ", True: " << resa.second << std::endl;
-    // resa2 = tree2.insert(333);
-    // std::cout << "My2: " << *resa2.first << ", True: " << resa2.second << std::endl;
-    
-    // std::cout << tree2.size() << "    size    "  << std::endl;
-    // std::cout << origin.size() << "   original size    "  << std::endl;
-
-    // tst = tree2.find(555);
-    // // tst = tree2.finder(555);
-    // original = origin.find(555);
-    // std::cout << "   find  555 " << *tst << std::endl;
-    // std::cout << "   find  original   " << *original << std::endl;
-    // tst = tree2.find(550);
-    // std::cout << "   find  550..." << *tst << std::endl;
-    // AVLTree<int>::NewIdeaIterator win;
-    // win = tree2.find(10);
-    // std::cout << "   find  10..." << *win << std::endl;
-    // original = origin.find(10);
-    // std::cout << "   find  original   " << *original << std::endl;
-    // original = origin.find(555);
-    // win = tree2.finder(250);
-    // std::cout << "   find  original   " << *original << std::endl;
-    // tree2.dump4(tree2.root);
-
-    // tree2.erase(5);
-    // // std::cout << "   finder  " << *win << std::endl;
-    // std::cout << tree2.size() << "    size    "  << std::endl;
-    // std::cout << origin.size() << "   original size    "  << std::endl;
-
-    // AVLTree<int> ypoc = tree2;
-    // // ypoc.root = xerox(tree2.root);
-    // // tree2.dump4(tree2.root);
-    // AVLTree<int> vlt{tree2};
-    // vlt.dump4(vlt.root);
-    // pinus.dump4(pinus.root);
-    // tree2.dump4(tree2.root);
-    // BinTreeIterator oak;
-
-    // std::cout << *tst << "  Now i can find smth " << std::endl;
-    // std::cout << *original << "  Original can find smth " << std::endl;
-    // origin.
 
     return 0;
 }
